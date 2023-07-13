258a33b55e669d379fbd2bf7262311d785ba9b11
Ochiengmorris <morris@UBUNTU.myguest.virtualbox.org>
2023-07-14 01:13:47 +0300

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char Ownstr[] = "\"Programming is like building a multilingual puzzle";
