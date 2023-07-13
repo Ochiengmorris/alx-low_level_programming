0af54e80ca80b2e343beff7c4d119ed890d4646f
Ochiengmorris <morris@UBUNTU.myguest.virtualbox.org>
2023-07-14 02:25:30 +0300

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	long int e;
 	long long int a;
 
-	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(e));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(a));
