2946fabf048e77345097014aac4faf4c6fd439a3
Ochiengmorris <morris@UBUNTU.myguest.virtualbox.org>
2023-07-14 23:43:50 +0300

    8-print_baze16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - entry point printing base 16 digits
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+		char baze16;
+
+		for (baze16 = '0'; base16 <= '9'; baze16++)
+		putchar(baze16);
+
+		for (baze16 = 'a'; baze16 <= 'f'; baze16++)
+		putchar(baze16);
+
+		putchar('\n');
+
+	return (0);
+}
