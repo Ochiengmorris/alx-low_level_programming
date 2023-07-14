18bf03ed5e1a6661a4f96b33c681915ac39fb412
Ochiengmorris <morris@UBUNTU.myguest.virtualbox.org>
2023-07-14 23:16:45 +0300

    5-printnumbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int baze10;
+
+	for (baze10 = 0; baze10 < 10; baze10++)
+	printf("%i", baze10);
+	putchar('\n');
+
+	return (0);
+}
