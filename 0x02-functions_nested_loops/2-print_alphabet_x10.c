6f9d6c3d20b8c5be6f5b72212c73f90bd6337af4
Ochiengmorris <morris@UBUNTU.myguest.virtualbox.org>
2023-07-19 00:28:19 +0300

    x10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 
+ *
+ * Return - success
+ */
+
+void print_alphabet_x10(void)
+{
+	int c = 0;
+	while (c < 10)
+	{
+		char d = 'a';
+		while (d <= 'z')
+		{
+			_putchar(d);
+			d++;
+		}	
+	_putchar('\n');
+	c++;
+	}	
+}
