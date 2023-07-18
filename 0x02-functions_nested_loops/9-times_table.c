f2763f0db2e7efe345a713571841239e0538dd95
Ochiengmorris <morris@UBUNTU.myguest.virtualbox.org>
2023-07-19 01:14:21 +0300

    9main

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *times_table - print 9 times table
+ *
+ *return 0 success
+ *
+ */
+void time_table(void)
+{
+int k, l, m;
+for (k = 0; k < 10; k++)
+{
+for (l = 0; l < 10; l++)
+{
+m = l * k;
+if (l == 0)
+{
+_putchar(m + '0');
+}
+if (m < 10 && l != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(m + '0');
+}
+else if (m >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
