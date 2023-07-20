e7d7a4192a25558772b99acd676542b5da76c0d1
Ochiengmorris <morris@UBUNTU.myguest.virtualbox.org>
2023-07-21 01:32:00 +0300

    3print

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,8 +3,8 @@
 
 /**
  * mul - Multiply 2 integers.
- * @a: An integer to be multiplied with b
- * @b : An integer to be multipled with a
+ * @c: An integer to be multiplied with b
+ * @d : An integer to be multipled with a
  * Return: Return the result of the multiplication
  */
 int mul(int c, int d)
