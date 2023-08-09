fd1cf442070ff84351ee9e60f6f9c35437111f29
johnmorris <oduyajohn66@gmail.com>
2023-08-10 01:03:04 +0300

    malloc 1

---
+++ B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: stored char
+ * Return: pointer of an array of chars
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *cd;
+	unsigned int i;
+	if (size == 0)
+	{
+		return NULL;
+	}
+	if (cd == NULL)
+	{
+		return NULL;
+	}
+	for (i = 0; i < size; i++)
+	{
+		cd[i] = c;
+
+		return NULL;
+	}
+}
---
+++ B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Mallocz=free
