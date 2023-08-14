81be276bb964adf8701737669cead47e94ce0b83
johnmorris <oduyajohn66@gmail.com>
2023-08-15 01:10:35 +0300

    2main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+	_putchar(__FILE__);
+	_putchar('\n');
+	return 0;
+}
---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * _putchar - prints the character x to stdout
+ * @x: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+ 
+void _putchar(char x)
+{
+	printf("%c", x);
+}
