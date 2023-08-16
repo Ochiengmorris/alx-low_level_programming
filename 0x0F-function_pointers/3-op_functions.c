0cffaa7f99dd079d14ef974860f51829edacce0e
johnmorris <oduyajohn66@gmail.com>
2023-08-16 23:54:20 +0300

    calccode

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,74 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - add func
+ * @a: variable
+ * @b: variable
+ *
+ * Return: ...
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract func
+ * @a: variable
+ * @b: variable
+ *
+ * Return: ...
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply variable
+ * @a: variable
+ * @b: variable
+ *
+ *  Return: ...
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division func
+ * @a: variable
+ * @b: variable
+ *
+ *  Return: ...
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder func
+ * @a: variable
+ * @b: variable
+ *
+ * Return: ...
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
