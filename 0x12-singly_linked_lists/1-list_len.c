cb0ef3d77254549f06610bb369f85b793410d742
johnmorris <oduyajohn66@gmail.com>
2023-08-26 23:45:32 +0300

    fix betty spaces

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,23 +1,22 @@
 #include "lists.h"
 
 /**
- * list_len - returns number of all elements in a list.
+ * list_len - returns the numbers of elements in list_t
+ * @h: constant pointer type list_t
  *
- * @h: pointer to the const list list_t.
- *
- * Return list.
+ * Return: numbers of elements in list_t
  */
 
 size_t list_len(const list_t *h)
 {
-    size_t n;
+size_t n;
 
-    n = 0;
+n = 0;
 
-    while (h != NULL)
-    {
-        n++;
-        h = h -> next;
-    }
-    return (n);
+while (h != NULL)
+{
+n++;
+h = h->next;
+}
+return (n);
 }
