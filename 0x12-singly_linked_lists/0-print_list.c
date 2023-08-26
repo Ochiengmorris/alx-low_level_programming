7dea07ba5243c77a724214f974fed9162a19ea68
johnmorris <oduyajohn66@gmail.com>
2023-08-26 23:30:53 +0300

    fix betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,17 @@
 #include "lists.h"
 
-/***/
+/**
+ * print_list - function that prints all the elements of a list.
+ *
+ * @h: is a pointer to the list_t (also, it is constant.)
+ *
+ * Return: number of elements
+ */
 
 size_t print_list(const list_t *h)
 {
 	size_t n;
+
 	n = 0;
 
 	while (h != NULL)
