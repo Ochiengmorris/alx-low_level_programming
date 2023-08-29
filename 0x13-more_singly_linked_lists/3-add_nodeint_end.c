6e2c492eac3a49439432bdc689237a5bfc9cd7a4
johnmorris <oduyajohn66@gmail.com>
2023-08-29 18:05:30 +0300

    add_node_end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/***/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *mov = *head;
+	listint_t *newend;
+
+	newend = malloc(sizeof(listint_t));
+
+	if (newend != NULL)
+	{
+		newend->n = n;
+		newend->next = NULL;
+	}
+	else
+	{
+		return (NULL);
+	}
+	if (mov != NULL)
+	{
+		while (mov->next != NULL)
+		{
+			mov = mov->next;
+		}
+		mov->next = newend;
+	}
+	else
+	{
+		*head = newend;
+	}
+	return (newend);
+}
