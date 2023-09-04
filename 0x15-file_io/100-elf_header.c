e681b0563ca280f66d743e0b2d3ad791d3fcb96f
johnmorris <oduyajohn66@gmail.com>
2023-09-05 00:20:34 +0300

    advanced

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,10 @@
-#include "main.h"
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void check_ifelf(unsigned char *e_ident);
 void prints_magic(unsigned char *e_ident);
@@ -65,7 +71,7 @@ void prints_magic(unsigned char *e_ident)
  */
 void prints_class(unsigned char *e_ident)
 {
-	printf("  Class:                        ");
+	printf("  Class:			     ");
 
 	switch (e_ident[EI_CLASS])
 	{
